fcf7f9421503cf5047ef177ba2a24c899f7836e3
Matthew Leventi <mleventi@lyft.com>
2015-11-25 12:14:07 -0800

    fixes

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM dregistry.lyft.net/base:latest
 ENTRYPOINT ["/usr/bin/nutcracker", "-c", "/etc/nutcracker/nutcracker.yaml"]
-ADD nutcracker /usr/bin/nutcracker
+# ADD nutcracker /usr/bin/nutcracker
+ADD source /etc/nutcracker
+RUN ls -la /etc/nutcracker
---
+++ conf/generate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import jinja2
+import yaml
+pools = None
+with open('pools.yaml', 'r') as fp:
+    pools = yaml.load(fp)
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'),
+                         undefined=jinja2.StrictUndefined)
+development = env.get_template('template.yaml').render(pools=pools, service_instance="development")
+development = yaml.load(development)
+with open('development.yaml', 'w') as fp:
+    fp.write(yaml.dump(development, default_flow_style=False))
